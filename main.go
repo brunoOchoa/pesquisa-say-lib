e806965be2fd50bc101424181fd573710cc811bb
Bruno Ochoa <bruno.ochoa.3@globalhitss.com.br>
2025-06-24 23:05:32 -0300

    model and object

--- main.go
+++ main.go
@@ -4,16 +4,15 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/brunoOchoa/whatsapp-lib/config"
 	"github.com/brunoOchoa/whatsapp-lib/pkg/whatsapp"
 )
 
 func main() {
-	cfg := config.LoadConfig()
+	// cfg := config.LoadConfig()
 
-	client := whatsapp.NewClient(cfg)
+	// client := whatsapp.NewClient(cfg)
 
-	to := []string{"5521985421711", "5521997921747"}
+	// to := []string{"5521985421711"} // Substitua pelo número de telefone do destinatário
 	// msg := "Olá, mensagem depois de ter respondido a mensagem anterior!"
 
 	// err := client.SendTextMessage(to, msg)
@@ -21,19 +20,63 @@ func main() {
 	// 	log.Fatalf("Erro ao enviar mensagem: %v", err)
 	// }
 
-	sendT := client.SendTemplateMessage(to, "hello_world", "en_US")
-	if sendT != nil {
-		log.Fatalf("Erro ao enviar template: %v", sendT)
-	}
+	// sendT := client.SendTemplateMessage(to, "hello_world", "en_US")
+	// if sendT != nil {
+	// 	log.Fatalf("Erro ao enviar template: %v", sendT)
+	// }
 
-	fmt.Println("✅ Mensagem enviada com sucesso!")
+	// fmt.Println("✅ Mensagem enviada com sucesso!")
 
 	// Exemplo de uso do GetMessageStatus
 	// Substitua pelo ID real retornado ao enviar uma mensagem
-	messageID := "SEU_MESSAGE_ID_AQUI"
-	status, err := client.GetMessageStatus(messageID)
-	if err != nil {
-		log.Fatalf("Erro ao buscar status da mensagem: %v", err)
+
+	webhookPayload := map[string]interface{}{
+		"object": "whatsapp_business_account",
+		"entry": []interface{}{
+			map[string]interface{}{
+				"id": "1235292671536231",
+				"changes": []interface{}{
+					map[string]interface{}{
+						"value": map[string]interface{}{
+							"messaging_product": "whatsapp",
+							"metadata": map[string]interface{}{
+								"display_phone_number": "15556576647",
+								"phone_number_id":      "650915048109117",
+							},
+							"statuses": []interface{}{
+								map[string]interface{}{
+									"id":           "wamid.HBgNNTUyMTk4NTE5MzUyNRUCABEYEjJFMTEyRkRBQjlBQzA5MjFGNwA=",
+									"status":       "failed",
+									"timestamp":    "1750773570",
+									"recipient_id": "5521985193525",
+									"errors": []interface{}{
+										map[string]interface{}{
+											"code":    131047,
+											"title":   "Re-engagement message",
+											"message": "Re-engagement message",
+											"error_data": map[string]interface{}{
+												"details": "Message failed to send because more than 24 hours have passed since the customer last replied to this number.",
+											},
+											"href": "https://developers.facebook.com/docs/whatsapp/cloud-api/support/error-codes/",
+										},
+									},
+								},
+							},
+						},
+						"field": "messages",
+					},
+				},
+			},
+		},
 	}
-	fmt.Printf("Status da mensagem %s: %s\n", messageID, status)
+	infos, err := whatsapp.ParseStatusFromWebhook(webhookPayload)
+	if err != nil {
+		log.Println("Erro ao processar webhook:", err)
+	}
+	for _, info := range infos {
+		fmt.Printf("Mensagem %s para %s está com status: %s\n", info.MessageID, info.RecipientID, info.Status)
+		if len(info.Errors) > 0 {
+			fmt.Printf("Erros: %+v\n", info.Errors)
+		}
+	}
 }
---
+++ pkg/model/webhook.go
@@ -0,0 +1,43 @@
+package model
+
+type Webhook struct {
+	Object string  `json:"object"`
+	Entry  []Entry `json:"entry"`
+}
+
+type Entry struct {
+	ID      string   `json:"id"`
+	Changes []Change `json:"changes"`
+}
+
+type Change struct {
+	Value Value  `json:"value"`
+	Field string `json:"field"`
+}
+
+type Value struct {
+	MessagingProduct string   `json:"messaging_product"`
+	Metadata         Metadata `json:"metadata"`
+	Statuses         []Status `json:"statuses"`
+}
+
+type Metadata struct {
+	DisplayPhoneNumber string `json:"display_phone_number"`
+	PhoneNumberID      string `json:"phone_number_id"`
+}
+
+type Status struct {
+	ID          string        `json:"id"`
+	Status      string        `json:"status"`
+	Timestamp   string        `json:"timestamp"`
+	RecipientID string        `json:"recipient_id"`
+	Errors      []StatusError `json:"errors,omitempty"`
+}
+
+type StatusError struct {
+	Code      int                    `json:"code"`
+	Title     string                 `json:"title"`
+	Message   string                 `json:"message"`
+	ErrorData map[string]interface{} `json:"error_data"`
+	Href      string                 `json:"href"`
+}
